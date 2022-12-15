d9b776a635822dc90b4add3accd56f193bf6ba23
一名普通大叔 <118505003+Lirongfei12@users.noreply.github.com>
2022-12-16 00:03:33 +0800

    Add files via upload
    turtle_circle 绘制圆

---
+++ turtle_circle.py
@@ -0,0 +1,43 @@
+##画圆
+# import turtle as t
+# from time import sleep
+# t.circle(100,360)
+# t.reset()#清屏
+# sleep(2)
+# t.penup()
+# t.goto(0,-250)
+# t.pendown()
+# raduis = 300
+# for i in range(4):
+#     t.circle(raduis,360)
+#     raduis -=30
+# sleep(2)
+# t.reset()#清屏
+# t.penup()
+# t.goto(0,-250)
+# t.pendown()
+# raduis = 300
+# for i in range(4):
+#     if raduis == 300:
+#         t.fillcolor('orange')
+#         t.begin_fill()
+#         t.circle(raduis, 360)
+#
+#     if raduis == 270:
+#         t.fillcolor('yellow')
+#         t.begin_fill()
+#         t.circle(raduis, 360)
+#
+#     if raduis == 240:
+#         t.fillcolor('pink')
+#         t.begin_fill()
+#         t.circle(raduis, 360)
+#
+#     if raduis == 210:
+#         t.fillcolor('gray')
+#         t.begin_fill()
+#         t.circle(raduis, 360)
+#
+#     t.end_fill()
+#     raduis -=30
+# t.done()
